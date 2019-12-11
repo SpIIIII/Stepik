1299b06fcf837a921dcac4f2a9a89034ddd9c44e
SpIIIII <Shipin16@gmail.com>
2019-12-11 23:25:43 +0300

    daily

--- Neural_Networks/HW_3/cars/world.py
+++ Neural_Networks/HW_3/cars/world.py
@@ -26,13 +26,13 @@ class World(metaclass=ABCMeta):
 
 
 class SimpleCarWorld(World):
-    COLLISION_PENALTY =  # выберите сами
-    HEADING_REWARD =  # выберите сами
-    WRONG_HEADING_PENALTY =  # выберите сами
-    IDLENESS_PENALTY =  # выберите сами
-    SPEEDING_PENALTY =  # выберите сами
-    MIN_SPEED =  # выберите сами
-    MAX_SPEED =  # выберите сами
+    COLLISION_PENALTY = 0 * 1e0
+    HEADING_REWARD = 0 * 1e-1
+    WRONG_HEADING_PENALTY = 0 * 1e0
+    IDLENESS_PENALTY = 32 * 1e-1
+    SPEEDING_PENALTY = 32 * 1e-1
+    MIN_SPEED = 0.1 * 1e0
+    MAX_SPEED = 0.7 * 1e0  # выберите сами
 
     size = (800, 600)
 
@@ -140,7 +140,7 @@ class SimpleCarWorld(World):
             self.visualize(scale)
             if self._update_display() == pygame.QUIT:
                 break
-            sleep(0.1)
+            # sleep(0.1)
 
         for i, agent in enumerate(self.agents):
             try:
---
+++ Neural_Networks/HW_3/script.sh
@@ -0,0 +1,23 @@
+
+while :
+do
+    python3 run_car.py -s 800 --seed 15 -f network_config_agent_0_layers_11_5_1.txt
+    sleep 18
+    python3 run_car.py -s 800 --seed 21 -f network_config_agent_0_layers_11_5_1.txt
+    sleep 18
+    python3 run_car.py -s 800 --seed 42 -f network_config_agent_0_layers_11_5_1.txt
+    sleep 18
+    python3 run_car.py -s 800 --seed 18 -f network_config_agent_0_layers_11_5_1.txt
+    sleep 18
+    python3 run_car.py -s 800 --seed 68 -f network_config_agent_0_layers_11_5_1.txt
+    sleep 18
+    python3 run_car.py -s 800 --seed 20 -f network_config_agent_0_layers_11_5_1.txt
+    sleep 18
+    python3 run_car.py -s 800 --seed 12 -f network_config_agent_0_layers_11_5_1.txt
+    sleep 18
+    python3 run_car.py -s 800 --seed 11 -f network_config_agent_0_layers_11_5_1.txt
+    sleep 18
+    python3 run_car.py -s 800 --seed 1 -f network_config_agent_0_layers_11_5_1.txt
+    sleep 18
+    
+done
