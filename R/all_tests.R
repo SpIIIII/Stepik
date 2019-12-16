efa83e24a0f090bd40eb884ec6941f8eb1301d76
SpIIIII <Shipin16@gmail.com>
2019-12-16 16:53:29 +0300

    daily

--- R/all_tests.R
+++ R/all_tests.R
@@ -1,3 +1,4 @@
+setwd('~/Programming/Stepik/R')
 ?AirPassengers
 str(AirPassengers)
 air_pas <- AirPassengers
@@ -45,7 +46,49 @@ sum(is.na(df))
 df$mpg[1:10] <- NA
 mean(df$mpg, na.rm = TRUE)
 aggregate(mpg ~ am, df, sd)
-x <- subset(airquality, Month == c(7,8,9))
-aggregate(Ozone ~ Month, x, length, na.action = na.pass())
-x <- 5
-> x %in% c(3, 4, 5)
+x <- subset(airquality, Month %in% c(7,8,9))
+aggregate(Ozone ~ Month, x, length,na.action = na.exclude)
+subset(airquality, Month %in% c(7,8,9))
+subset(airquality,Month == c(7,8))
+
+?describeBy
+str(airquality)
+describeBy(airquality,group = airquality$Month)
+
+describe(iris[iris$Species %in% "virginica",] )
+iris[iris$Species %in% "virginica"]
+iris
+x <-c(23, 10, 16, 19, 23, 22, 16, 21, 24, 20, 22, 21, 19, 25, 22, 14, 22, 14, 16, 15, NA, 24, NA, NA, NA, 23, 15, 21, 24, 
+      NA, NA, NA, 18, 21, 18, NA, 17, 20, 17, NA)
+
+# graphics
+
+hist(df$mpg,breaks = 20, xlab = "MPG")
+boxplot(mpg ~ am, df)
+plot(df$mpg,df$hp)
+
+library(ggplot2)
+ggplot(df,aes(x = mpg)) + geom_histogram(fill = "white", col = "black", binwidth = 2)
+
+ggplot(df,aes(x = mpg, fill = am)) + geom_dotplot()
+ggplot(df,aes(x = mpg, fill = am)) + geom_density(alpha = 0.2)
+ggplot(df, aes(x=am, y = hp, col = vs))+geom_boxplot()
+my_plot <- ggplot(df, aes(x=mpg, y=hp, col = vs, size = qsec))+geom_point()
+my_plot2 <-ggplot(df,aes(x = mpg, fill = am))
+my_plot2+geom_dotplot()
+ggplot(airquality,aes(x=as.factor(Month), y=Ozone)) + geom_boxplot()
+plot1 <- ggplot(mtcars,aes(x=mpg, y=disp, col=hp)) + geom_point()
+ggplot(iris, aes(Sepal.Length)) + geom_histogram(aes(fill = Species),alpha = 0.7) 
+
+write.csv(airquality,'airq.csv')
+getwd()
+
+
+
+
+
+
+
+
+
+
