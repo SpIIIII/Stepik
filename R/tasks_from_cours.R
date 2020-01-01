bc18846068752633a46ecdf9850866e35dc9c7a4
SpIIIII <Shipin16@gmail.com>
2020-01-01 21:54:27 +0300

    daily R

--- R/tasks_from_cours.R
+++ R/tasks_from_cours.R
@@ -725,24 +725,69 @@ y
 
 # 3.6.3 - 3.6.4
 my_df <- read.csv('train.csv', sep = ';')
+str(my_df)
+ggplot(my_df, aes(read, math, col = gender))+
+  geom_point()+
+  facet_grid(.~hon)+
+  theme(axis.text = element_text(size = 25),
+        axis.title=element_text(size=25, face = 'bold'))
 
+fit <- glm(hon~ read+math+gender, my_df, family = "binomial")
+summary(fit)
 
+head(predict(object = fit, type = 'response'))
+my_df$prob <- predict(object = fit, type = 'response')
 
+# 3.6.5
+log_coef <- glm(am~disp+vs+mpg, mtcars,family = "binomial")
+log_coef$coefficients
 
+# 3.6.6
+ggplot(data = ToothGrowth, aes(x = supp, y = len, fill = factor(dose)))+
+  geom_boxplot()
 
+# 3.6.7 - 3.6.8
+library(ROCR)
+pred_fit <- prediction(my_df$prob,my_df$hon)
+perf_fit <- performance(pred_fit, 'tpr', 'fpr')
+plot(perf_fit, colorize =T, print.cutoffs.at = seq(0,1,by=0.1))
+auc <- performance(pred_fit, measure = 'auc')
+str(auc)
+perf3 <- performance(pred_fit, x.measure = 'cutoff', measure = 'spec')
+perf4 <- performance(pred_fit, x.measure = 'cutoff', measure = 'sens')
+perf5 <- performance(pred_fit, x.measure = 'cutoff', measure = 'acc')
 
+plot(perf3, col = 'red',lwd =2)
+plot(add=T, perf4, col = 'green',lwd =2)
+plot(add = T, perf5, lwd=2)
 
+abline (v=0.225, lwd =2)
 
+my_df$pred_resp <- factor(ifelse(my_df$prob > 0.225, 1, 0),labels = c('N','Y'))
+my_df$correct <- ifelse(my_df$pred_resp == my_df$hon,1,0)
+ 
+ggplot(my_df, aes(prob, fill = factor(correct)))+
+  geom_boxplot()+
+  theme(axis.text = element_text(size=25),
+        axis.title=element_text(size=25,face='bold'))
 
+mean(my_df$correct)
+test_df <- read.csv('test.csv',sep = ';')
+test_df$hon <- NA
 
+test_df$hon <- predict(fit, newdata = test_df, type = 'response')
+View(test_df)
 
+# 3.6.9
+data <- read.csv('data.csv')
+str(data)
+fit <- glm(admit ~ gpa*rank, data, family = "binomial")
 
+data_na <- data[is.na(data$admit),]
+data_na$admit <- predict(fit,data_na[,c(2,3,4)],type = 'response')
+nrow(data_na[data_na$admit > 0.4,])
 
-
-
-
-
-
+# 3.7.2
 
 
 
