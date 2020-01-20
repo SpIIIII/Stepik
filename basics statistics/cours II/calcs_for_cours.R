b9fc4a66fd5967bd0c999c608fecd61375953567
SpIIIII <Shipin16@gmail.com>
2020-01-20 23:35:14 +0300

    daily basics statistics

--- basics statistics/cours II/calcs_for_cours.R
+++ basics statistics/cours II/calcs_for_cours.R
@@ -1,1 +1,12 @@
 setwd('~/Programming/Stepik/basics statistics/cours II')
+
+df = data.frame(c(10,6),c(5,15))
+df
+res <- chisq.test(df)
+str(res)
+res$expected
+df = data.frame(c(6,0),c(3,3))
+chisq.test(df)
+df = data.frame(c(20,15),c(11,12))
+chisq.test(df)
+                
